5e951f8556c19161bde8c2c384b92de3fed4d241
ShinjiKimuradascap <shinji@dascapital.io>
2026-01-27 21:46:53 +0800

    chore: remove accidental project files

--- haikyu_project/backend/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI(title="Haikyu!! Clone API")
-
-@app.get("/")
-async def root():
-    return {"message": "Welcome to Haikyu!! Clone Backend (Haidori Clone)"}
